a0dabfc49b1d2561f1a129d3cf32fc64097f2e87
umangagarwalit <umangagarwalit@gmail.com>
2019-05-25 18:10:51 +0530

    mycommit

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -3,11 +3,6 @@ package com.expense.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
-//import static org.testng.Assert.assertEquals;
-
-//import org.testng.annotations.Test;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
@@ -16,16 +11,13 @@ public class ExpenseManagerSystemTest {
 
 
 	@Test
-	public void titleTest() throws Exception{
+	public void titleTest(){
 		System.out.println("Start");
 		WebDriver driver;
 		driver = new HtmlUnitDriver();
-		//Thread.sleep(5000);
 		driver.get("http://localhost:9191");
 		String expectedTitle = "Login page";
 		String actualTitle = driver.getTitle();
-		System.out.println("Expected: " + expectedTitle);
-		System.out.println("Actual: " + actualTitle);
 		assertEquals(expectedTitle, actualTitle);
         System.out.println(actualTitle);
 	}
