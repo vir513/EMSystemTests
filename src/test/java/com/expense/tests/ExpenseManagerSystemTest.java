e8e659d4bdf3ed181276b12b94c3e976ecbcd189
umangagarwalit <umangagarwalit@gmail.com>
2019-05-25 15:19:34 +0530

    mycommit

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -2,24 +2,18 @@ package com.expense.tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 
 public class ExpenseManagerSystemTest {
- 
-WebDriver driver;
-	
-	@Before
-	public void setUp() throws Exception {
-	driver = new HtmlUnitDriver();
-	}
+
 
 	@Test
 	public void titleTest() {
+		WebDriver driver;
+		driver = new HtmlUnitDriver();
 		driver.get("http://localhost:9191");
 		String expectedTitle = "Login page";
 		String actualTitle = driver.getTitle();
@@ -27,9 +21,4 @@ WebDriver driver;
         System.out.println(actualTitle);
 	}
 
-	
-	@After
-	public void tearDown() throws Exception {
-	}
-  
 }
