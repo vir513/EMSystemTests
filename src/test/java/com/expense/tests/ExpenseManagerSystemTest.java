e115196e4a53e103592f8a505d04db863ada3e9a
umangagarwalit <umangagarwalit@gmail.com>
2019-05-27 11:16:03 +0530

    mycommit

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -16,8 +16,12 @@ public class ExpenseManagerSystemTest {
 		System.out.println("Start1");
 		WebDriver driver;
 		driver = new HtmlUnitDriver();
-		driver.get("http://newtours.demoaut.com/");
-		String expectedTitle = "Welcome: Mercury Tours";
+		
+		//driver.get("http://newtours.demoaut.com/");
+		//String expectedTitle = "Welcome: Mercury Tours";
+		
+		driver.get("http://localhost:9191");
+		String expectedTitle = "Login page";
 		String actualTitle = driver.getTitle();
 		assertEquals(expectedTitle, actualTitle);
         System.out.println(actualTitle);
