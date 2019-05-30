c9f64eb2c67a1b95eeec61251ef57c5492e60b57
umangagarwalit <umangagarwalit@gmail.com>
2019-05-30 16:22:05 +0530

    codecommit1.0

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -16,8 +16,14 @@ public class ExpenseManagerSystemTest {
 		WebDriver driver;
 		driver = new HtmlUnitDriver();
 		Thread.sleep(10000);
-		driver.get("http://localhost:8089/ExpenseApp-1/login.jsp");		
-		String expectedTitle = "Login page";
+		driver.get("http://localhost:8089/ExpenseApp-1/login.jsp");	
+
+		//Comment below line to make test pass
+		String expectedTitle = "Hello Page";
+		
+		//Uncomment below line to make test pass
+		//String expectedTitle = "Login page";
+		
 		String actualTitle = driver.getTitle();
 		assertEquals(expectedTitle, actualTitle);
         System.out.println(actualTitle);
