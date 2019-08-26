0fff5bb679eb9a42157add93bb4bb6fed3d649b8
umangsaltuniv <umang@saltuniv.com>
2019-08-26 06:43:30 +0530

    codecommit1.0

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -19,10 +19,10 @@ public class ExpenseManagerSystemTest {
 		//Thread.sleep(600000);
 
 		//Comment below line to make test pass
-		//String expectedTitle = "Hello Page";
+		String expectedTitle = "Hello Page";
 		
 		//Uncomment below line to make test pass
-		String expectedTitle = "Login page";
+		//String expectedTitle = "Login page";
 		
 		String actualTitle = driver.getTitle();
 		assertEquals(expectedTitle, actualTitle);
